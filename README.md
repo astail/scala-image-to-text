b9f0f991bb1fbe4e770abfd5ca974ecf7237f02e
astel <kiyomillefeuille@gmail.com>
2018-04-05 14:06:11 +0900

    フォルダの中を解析するようにした

---
+++ README.md
@@ -0,0 +1,23 @@
+## setup
+
+`brew install tesseract`     
+
+## result
+
+./file/astail.png
+
+```
+hnns://as15il.ne‘
+```
+
+./file/astel.png
+
+```
+ASTEL
+QStGJ
+```
+
+./file/pubg_s.png
+```
+mm-un-m
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,27 @@ import java.io.File
 import net.sourceforge.tess4j.{Tesseract, TesseractException}
 
 object Main extends App {
-  println("aaaa")
-  piyo
 
-  def piyo: Unit = {
-    val imageFile = new File("./file/astel.png")
+  val DIR = "./file"
+  val filename = getListOfFiles(DIR)
+
+  for(x <- filename) {
+    println(x)
+    kaiseki(x.toString)
+  }
+
+  //https://alvinalexander.com/scala/how-to-list-files-in-directory-filter-names-scala
+  def getListOfFiles(dir: String):List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+
+  def kaiseki(filename: String) = {
+    val imageFile = new File(filename)
     val instance = new Tesseract()
     instance.setDatapath(System.getenv("TESSDATA_PREFIX"))
     try {
@@ -17,5 +33,4 @@ object Main extends App {
       case e: TesseractException => Console.err.println(e.getMessage())
     }
   }
-
 }
